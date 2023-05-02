5a8ad00faac6a8ebf15c5d653e44992b8d44f46f
Maksim <vasilyev.v.maxim@gmail.com>
2023-05-02 13:10:26 -0600

    Unnecessary files removed from tracking

--- server/routes/jokeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const authController = require('../controllers/authController');
-
-router.route('/').get(authController.protect);
-
-
-module.exports = router;
